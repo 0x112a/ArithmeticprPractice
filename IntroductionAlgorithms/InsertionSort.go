51a7736b3c5b411d48be3143cf0ad178a2ad01bd
0x112 <1436063244@qq.com>
2020-08-24 20:22:41 +0800

    add code

--- IntroductionAlgorithms/InsertionSort.go
+++ IntroductionAlgorithms/InsertionSort.go
@@ -4,16 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func solution(line string) string  {
+func solution(line string) string {
 	var aws string
+	key := strings.Split(line, " ")
+	n, _ := strconv.Atoi(key[1])
+	fmt.Println(n)
 	return aws
 }
 
-func main()  {
-	r := bufio.NewReaderSize(os.Stdin,20488)
-	for line,_,err := r.ReadLine(); err == nil; line,_,err = r.ReadLine(){
+func main() {
+	r := bufio.NewReaderSize(os.Stdin, 20488)
+	for line, _, err := r.ReadLine(); err == nil; line, _, err = r.ReadLine() {
 		fmt.Println(solution(string(line)))
 	}
 }
